7b5316721b3047bc69aff372fc67a2e25d0d83cc
Onurallp <onuralp.aslaner@ametrica.io>
2022-11-16 13:17:38 +0300

    a

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    demo_variable = 'hoşgeldiniz'
+    demo_variable = 'hi'
     return demo_variable
 
 @app.route('/name-username', methods=['POST'])
