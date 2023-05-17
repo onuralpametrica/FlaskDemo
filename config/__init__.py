3e6c8fcaf2f4cfe64d77483b80e21870e389c013
oytunametrica <116077443+oytunametrica@users.noreply.github.com>
2023-05-17 14:54:02 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 
 def get_database():
-    connection_string = "mongodb+srv://Oytunaa:Data-analisti-543@cluster0.qevwqxp.mongodb.net/test"
+    connection_string = ""
     client = MongoClient(connection_string)
     return client['Register']
 
